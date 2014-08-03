3729454ec3242d252dedcb9bc0beda5d9414e16c
Manikandan <rkmanikarthick@gmail.com>
2014-08-03 23:16:21 +0530

    Handled nested objects scenario

--- js/jsondiff.js
+++ js/jsondiff.js
@@ -65,14 +65,15 @@ var compare = function (json1, json2) {
 	var keys = _.union(Object.keys(json1), Object.keys(json2)) ;
 	keys.forEach(function(key){
 		if(json1.hasOwnProperty(key) || json2.hasOwnProperty(key) ) {
-			if( typeof(json1[key]) == 'object' || typeof(json2[key]) == 'object') {
-				
+			if( typeof(json1[key]) == 'object' && typeof(json2[key]) == 'object' ) {
 				var deepObjectDiffs = compare(json1[key], json2[key]);
 				if(! isEmpty(deepObjectDiffs[0])) {
 				diff1[key] = deepObjectDiffs[0];
 				diff2[key] = deepObjectDiffs[1]; 
 				}
-
+			}else if (typeof(json1[key]) == 'object' || typeof(json2[key]) == 'object') {
+				diff1[key] = json1[key];
+				diff2[key] = json2[key];
 			}else if(json1[key] != json2[key]) {
 				diff1[key] = json1[key];
 				diff2[key] = json2[key];
